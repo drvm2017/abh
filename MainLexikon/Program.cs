cbd33b30a8a6c7b3120fae7514c8d4b6cf4bb9d6
Dr. Volker Mühlenkamp <dr.muehlenkamp@givo-ifbb.de>
2020-01-29 00:20:16 +0100

    Hinzufügen von Worten zu einem leeren Lexikon

---
+++ MainLexikon/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainLexikon
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      String[] worte;
+      Wort wort;
+      Lexikon lexikon = new Lexikon();
+      String eingabe=Console.ReadLine();
+      worte = eingabe.Split(new String[] { ",", ".", ";", ":", " " }, StringSplitOptions.RemoveEmptyEntries);
+
+      for (int i = 0; i < worte.Length; i++)
+      {
+        wort = new Wort(worte[i]);
+        if (lexikon.Contains(wort))
+        {
+
+        }
+        else
+        {
+          lexikon.Add(wort);
+        }
+      }
+      Console.ReadLine();
+    }
+  }
+}
---
+++ MainLexikon/Wort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainLexikon
+{
+  public class Wort:IEquatable<Wort>
+  {
+    private String wort;
+
+    public Wort(String wort)
+    {
+      this.wort = wort;
+    }
+
+    public override string ToString()
+    {
+      return "Wort: " + wort;
+    }
+    public override bool Equals(object obj)
+    {
+      if (obj == null) return false;
+      Wort objAsWort = obj as Wort;
+      if (objAsWort == null) return false;
+      else return Equals(objAsWort);
+    }
+    public override int GetHashCode()
+    {
+      return (new Random()).Next(0, 2 ^ 32-1);
+    }
+    public bool Equals(Wort other)
+    {
+      if (other == null) return false;
+      return this.wort == other.wort;
+    }
+  }
+}
